9b4ecd1fb174ac675da015a5f5f3e22d9e89013e
Nilay-Thakkar <nilayr.thakkar@gmail.com>
2020-07-24 09:38:53 +0530

    website

---
+++ header.php
@@ -0,0 +1,44 @@
+        <header id="topnav" class="defaultscroll sticky border-bottom">
+            <div class="container">
+                <!-- Logo container-->
+                <div>
+                    <a class="logo" href="index.php">
+                        <img src="images/logo-dark.png" height="30" alt="">
+                    </a>
+                </div>                 
+                <div class="buy-button">
+                    <a href="signin.php" target="_blank" class="btn btn-outline-primary">Sign In</a>
+                </div><!--end login button-->
+                <div class="buy-button">
+                    <a href="signup.php" target="_blank" class="btn btn-primary">Sign Up</a>
+                </div><!--end login button-->
+                <!-- End Logo container-->
+                <div class="menu-extras">
+                    <div class="menu-item">
+                        <!-- Mobile menu toggle-->
+                        <a class="navbar-toggle">
+                            <div class="lines">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </a>
+                        <!-- End mobile menu toggle-->
+                    </div>
+                </div>
+        
+                <div id="navigation">
+                    <!-- Navigation Menu-->   
+                    <ul class="navigation-menu">
+                        <li><a href="index.php"><i class="mdi mdi-home-outline"></i> Home Page</a></li>
+                        <li><a href="blog.php"><i class="mdi mdi-file-document-outline"></i> Our Blogs</a></li>
+                        <li><a href="help.php"><i class="mdi mdi-help-circle-outline"></i> Help Desk</a></li>
+                        <li><a href="contact.php"><i class="mdi mdi-contacts-outline"></i> Contact Us</a></li>
+                    </ul><!--end navigation menu-->
+                    <div class="buy-menu-btn d-none">
+                        <a href="signin.php" target="_blank" class="btn btn-primary">Sign In</a>
+                        <a href="signup.php" target="_blank" class="btn btn-primary-outline">Sign Up</a>
+                    </div><!--end login button-->
+                </div><!--end navigation-->
+            </div><!--end container-->
+        </header><!--end header-->
