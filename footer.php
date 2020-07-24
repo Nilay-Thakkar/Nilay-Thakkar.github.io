1890809babdd68088d7dcbc46bb2600e71c05ef9
Nilay-Thakkar <68693236+Nilay-Thakkar@users.noreply.github.com>
2020-07-24 08:52:01 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,81 @@
+ <footer class="footer border-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-12 mb-0 mb-md-4 pb-0 pb-md-2">
+                        <a href="#" class="logo-secondaryer">
+                            <img src="images/logo-dark.png" height="30" alt="">
+                        </a>
+                        <p class="mt-4 text-secondary">Mom and Paw is an innovative pet health and wellness brand providing dogs with fresh, human-grade meals, delivered directly to consumers. Our mission is to deliver freshly prepared meals for your dog made with Mother's love.</p>
+                        <ul class="list-unstyled social-icon social mb-0 mt-4">
+                            <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i data-feather="facebook" class="fea icon-sm fea-social"></i></a></li>
+                            <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i data-feather="instagram" class="fea icon-sm fea-social"></i></a></li>
+                            <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i data-feather="twitter" class="fea icon-sm fea-social"></i></a></li>
+                            <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i data-feather="linkedin" class="fea icon-sm fea-social"></i></a></li>
+                        </ul><!--end icon-->
+                    </div><!--end col-->
+                    
+                    <div class="col-lg-2 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                        <h4 class="text-dark footer-head">Company</h4>
+                        <ul class="list-unstyled footer-list mt-4">
+                            <li><a href="" class="text-secondary"><i class="mdi mdi-chevron-right mr-1"></i> About us</a></li>
+                            <li><a href="" class="text-secondary"><i class="mdi mdi-chevron-right mr-1"></i> Services</a></li>
+                            <li><a href="" class="text-secondary"><i class="mdi mdi-chevron-right mr-1"></i> Team</a></li>
+                            <li><a href="" class="text-secondary"><i class="mdi mdi-chevron-right mr-1"></i> Pricing</a></li>
+                            <li><a href="" class="text-secondary"><i class="mdi mdi-chevron-right mr-1"></i> Project</a></li>
+                        </ul>
+                    </div><!--end col-->
+                    
+                    <div class="col-lg-3 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                        <h4 class="text-dark footer-head">Usefull Links</h4>
+                        <ul class="list-unstyled footer-list mt-4">
+                            <li><a href="" class="text-secondary"><i class="mdi mdi-chevron-right mr-1"></i> Terms of Services</a></li>
+                            <li><a href="" class="text-secondary"><i class="mdi mdi-chevron-right mr-1"></i> Privacy Policy</a></li>
+                            <li><a href="" class="text-secondary"><i class="mdi mdi-chevron-right mr-1"></i> Documentation</a></li>
+                            <li><a href="" class="text-secondary"><i class="mdi mdi-chevron-right mr-1"></i> Changelog</a></li>
+                            <li><a href="" class="text-secondary"><i class="mdi mdi-chevron-right mr-1"></i> Components</a></li>
+                        </ul>
+                    </div><!--end col-->
+
+                    <div class="col-lg-3 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                        <h4 class="text-dark footer-head">Newsletter</h4>
+                        <p class="mt-4 text-secondary">Sign up and receive free health and nutrition tips via email.</p>
+                        <form>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="foot-subscribe form-group position-relative">
+                                        <label class="text-secondary">Write your email <span class="text-danger">*</span></label>
+                                        <i data-feather="mail" class="fea icon-sm icons"></i>
+                                        <input type="email" name="email" id="emailsubscribe" class="form-control pl-5 rounded" placeholder="Your email : " required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <input type="submit" id="submitsubscribe" name="send" class="btn btn-soft-primary btn-block" value="Subscribe">
+                                </div>
+                            </div>
+                        </form>
+                    </div><!--end col-->
+                </div><!--end row-->
+            </div><!--end container-->
+        </footer><!--end footer-->
+        <footer class="footer footer-bar text-center justify-content-center">
+            <div class="container text-center justify-content-center">
+                <div class="row justify-content-center">
+                    <div class="col-sm-12 text-center">
+                        <div class="text-sm-left text-center">
+                            <p class="mb-0 text-center text-secondary">© Copyrights 2020, Design with <i class="mdi mdi-heart text-danger"></i> by <a href="" target="_blank" class="text-reset">Mom and Paw Team</a>.</p>
+                        </div>
+                    </div><!--end col-->
+
+                    <div class="col-sm-6 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                       <!--  <ul class="list-unstyled text-sm-right mb-0">
+                            <li class="list-inline-item"><a href="javascript:void(0)"><img src="images/payments/american-ex.png" class="avatar avatar-ex-sm" title="American Express" alt=""></a></li>
+                            <li class="list-inline-item"><a href="javascript:void(0)"><img src="images/payments/discover.png" class="avatar avatar-ex-sm" title="Discover" alt=""></a></li>
+                            <li class="list-inline-item"><a href="javascript:void(0)"><img src="images/payments/master-card.png" class="avatar avatar-ex-sm" title="Master Card" alt=""></a></li>
+                            <li class="list-inline-item"><a href="javascript:void(0)"><img src="images/payments/paypal.png" class="avatar avatar-ex-sm" title="Paypal" alt=""></a></li>
+                            <li class="list-inline-item"><a href="javascript:void(0)"><img src="images/payments/visa.png" class="avatar avatar-ex-sm" title="Visa" alt=""></a></li>
+                        </ul> -->
+                    </div><!--end col-->
+                </div><!--end row-->
+            </div><!--end container-->
+        </footer><!--end footer-->
+       
